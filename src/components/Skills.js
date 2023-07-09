ac889d4e3d20600853fd5a639de1611877eea1ad
Aboli Wankhade <104322603+Aboli56@users.noreply.github.com>
2023-07-09 09:41:30 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -56,20 +56,20 @@ const Experience = () => {
     {
       id: 8,
       src: bootstrap,
-      title: "GitHub",
+      title: "Bootstrap",
       style: "shadow-gray-400",
     }
   ];
 
   return (
     <div
-      name="experience"
+      name="skills"
       className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
-            Experience
+            Technical Skills
           </p>
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
